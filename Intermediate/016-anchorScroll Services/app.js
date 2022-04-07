e610d9f51b294a5b707e1870fceb0121e032eaa7
Abrão Astério Junior <abcorinthians@gmail.com>
2022-04-07 12:12:15 -0300

    Using Built-in, Custom, and HTTP Services: Using anchorScroll Services

---
+++ Intermediate/016-anchorScroll Services/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+angular.module('myApp', []);
+
+angular.module('myApp').controller('MainController', ['$location', '$anchorScroll', function ($location, $anchorScroll) {
+
+    this.jumpToTop = function () {
+        $location.hash('topLink');
+        $anchorScroll();
+    };
+
+    this.jumpToBottom = function () {
+        $location.hash('bottomLink');
+        $anchorScroll();
+    }
+}]);
