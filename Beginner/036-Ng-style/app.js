b535f84aafdcb5c139b4c8d199eeeca88456d765
Abrão Astério Junior <abcorinthians@gmail.com>
2022-04-07 09:43:24 -0300

    Working with Display Directives: Using ngStyle Directives

---
+++ Beginner/036-Ng-style/app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+angular.module('myApp', []);
+
+angular.module('myApp').controller('MainController', [function () {
+
+    this.enableStyles = function () {
+        this.style1 = {
+            color: 'red',
+            fontSize: '16px'
+        };
+
+        this.style2 = {
+            color: 'blue',
+            fontSize: '10px'
+        };
+    }
+
+    this.disableStyles = function () {
+        this.style1 = null;
+        this.style2 = null;
+    }
+
+}]);
