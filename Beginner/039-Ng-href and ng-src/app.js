8c2dd1b3c56664665616cde2f95358555316857d
Abrão Astério Junior <abcorinthians@gmail.com>
2022-04-07 10:02:47 -0300

    Using ngHref and ngSrc Directives

---
+++ Beginner/039-Ng-href and ng-src/app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+angular.module('myApp', []);
+
+angular.module('myApp').controller('MainController', [function () {
+
+    this.imageUrl = '../030-NgMouseDown and ngMouseUp/spider-man-no-way-home.jpg';
+    this.linkUrl = 'http://www.angularjs.org';
+}]);
