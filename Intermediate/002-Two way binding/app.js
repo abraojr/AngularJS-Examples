ee9644499d835a3c40b531b57e776f505ab18e93
Abrão Astério Junior <abcorinthians@gmail.com>
2022-04-07 10:30:47 -0300

    Setting Up Two-way Binding

---
+++ Intermediate/002-Two way binding/app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+angular.module('myApp', []);
+
+angular.module('myApp').directive('tstVehicleView', function () {
+
+    return {
+        scope: {
+            item: '='
+        },
+        templateUrl: './templates/vehicle.html',
+        restrict: 'EA'
+    };
+});
+
+angular.module('myApp').controller('MainController', ['$scope', function ($scope) {
+
+    $scope.firstItem = {
+        year: 2012,
+        make: 'Mazda',
+        model: '6',
+        price: 18999
+    }
+
+    $scope.secondItem = {
+        year: 2015,
+        make: 'Lexus',
+        model: 'IS 250',
+        price: 39650
+    }
+}]);
