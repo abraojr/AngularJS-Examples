f1ad53010e0a7f9b1c9955172d42875ed875f257
Abrão Astério Junior <abcorinthians@gmail.com>
2022-04-07 09:46:30 -0300

    Using ngClass Directives

---
+++ Beginner/037-Ng-class/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+angular.module('myApp', []);
+
+angular.module('myApp').controller('MainController', [function () {
+
+    this.class1 = 'redClass';
+    this.class2 = 'greenClass';
+    this.isSelected = false;
+
+}]);
