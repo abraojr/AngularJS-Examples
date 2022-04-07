38e64f0cf142e3c63a96b5f527d7e5ad9c94b9ae
Abrão Astério Junior <abcorinthians@gmail.com>
2022-04-07 12:37:55 -0300

    Using cacheFactory Services

---
+++ Intermediate/021-cacheFactory Services/app.js
@@ -0,0 +1,27 @@
+"use strict";
+
+angular.module('myApp', []);
+
+angular.module('myApp').controller('MainController', ['$cacheFactory', function ($cacheFactory) {
+
+    var vm = this;
+
+    vm.keys = [];
+    vm.cache = $cacheFactory('testCache');
+
+    this.addItem = function (itemKey, itemValue) {
+        vm.keys.push(itemKey);
+        vm.cache.put(itemKey, itemValue);
+    };
+
+    this.getItem = function (itemKey) {
+        vm.currentItem = vm.cache.get(itemKey);
+    };
+
+    this.removeItem = function (itemKey) {
+        vm.keys = vm.keys.filter(function (key) {
+            return (key !== itemKey);
+        });
+        vm.cache.remove(itemKey);
+    };
+}]);
