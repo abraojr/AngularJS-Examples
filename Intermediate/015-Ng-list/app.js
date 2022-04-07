efe00a44a720a19f97d33bbf058370ee17cc1837
Abrão Astério Junior <abcorinthians@gmail.com>
2022-04-07 12:08:54 -0300

    Using ngList Directives

---
+++ Intermediate/015-Ng-list/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+angular.module('myApp', []);
+
+angular.module('myApp').controller('MainController', [function () {
+
+    this.names = ['David', 'Jill', 'John', 'Laura', 'Michael'];
+
+    this.getType = function (value) {
+        return typeof (value);
+    }
+}]);
