bf2cc4194e52b5581b0aed8419387e1e5499c9ee
Abrão Astério Junior <abcorinthians@gmail.com>
2022-04-07 00:12:29 -0300

    Using ngClick

---
+++ Beginner/026-Ng-click/app.js
@@ -0,0 +1,28 @@
+"use strict";
+
+angular.module('myApp', []);
+
+// THIS IS BAD 
+var mainCtrl = null;
+
+angular.module('myApp').controller('MainController', [function () {
+
+    mainCtrl = this;
+
+    var vm = this;
+
+    vm.clickCount = 0;
+
+    vm.updateClickCount = function (amount) {
+        vm.clickCount += amount;
+    }
+
+}]);
+
+// THIS IS BAD
+function externalClick(elem, amount) {
+
+    var $scope = angular.element(elem).scope();
+    mainCtrl.clickCount += parseInt(amount);
+    $scope.$apply();
+}
