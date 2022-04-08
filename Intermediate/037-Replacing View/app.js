6e9cb352d85119e1d28e31d5d43705ed1ae8d180
Abrão Astério Junior <abcorinthians@gmail.com>
2022-04-08 15:47:13 -0300

    Replacing Views

---
+++ Intermediate/037-Replacing View/app.js
@@ -0,0 +1,43 @@
+"use strict";
+
+angular.module('myApp', ['ngRoute']);
+
+angular.module('myApp').config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider
+        .when('/welcome', {
+            templateUrl: '../036-Client Site Routing/views/welcome.html',
+            controller: 'WelcomeController',
+            controllerAs: 'vm'
+        })
+        .when('/about', {
+            templateUrl: '../036-Client Site Routing/views/about.html',
+            controller: 'AboutController',
+            controllerAs: 'vm'
+        })
+        .otherwise({
+            redirectTo: '/welcome'
+        });
+}]);
+
+
+angular.module('myApp').controller('NavController', ['$location', function ($location) {
+
+    this.goto = function (path) {
+        $location.replace();
+        $location.path(path);
+    };
+
+}]);
+
+angular.module('myApp').controller('WelcomeController', [function () {
+
+    this.details = 'Hello, from WelcomeController.';
+
+}]);
+
+angular.module('myApp').controller('AboutController', [function () {
+
+    this.details = 'Hello, from AboutController.';
+
+}]);
